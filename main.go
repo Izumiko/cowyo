980461ac375b9a2dd9ae7d48f6ae631480fe9602
Songtao Chu <im@chu.st>
2020-08-12 21:16:25 +0800

    Update go.mod

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/jcelliott/lumber"
 	"github.com/schollz/cowyo/server"
 
-	cli "gopkg.in/urfave/cli.v1"
+	cli "github.com/urfave/cli/v2"
 )
 
 var version string
@@ -22,142 +22,144 @@ func main() {
 	app.Version = version
 	app.Compiled = time.Now()
 	app.Action = func(c *cli.Context) error {
-		pathToData = c.GlobalString("data")
+		pathToData = c.String("data")
 		os.MkdirAll(pathToData, 0755)
-		host := c.GlobalString("host")
-		crt_f := c.GlobalString("cert") // crt flag
-		key_f := c.GlobalString("key")  // key flag
+		host := c.String("host")
+		crtFlag := c.String("cert") // crt flag
+		keyFlag := c.String("key")  // key flag
 		if host == "" {
 			host = GetLocalIP()
 		}
 		TLS := false
-		if crt_f != "" && key_f != "" {
+		if crtFlag != "" && keyFlag != "" {
 			TLS = true
 		}
 		if TLS {
-			fmt.Printf("\nRunning cowyo server (version %s) at https://%s:%s\n\n", version, host, c.GlobalString("port"))
+			fmt.Printf("\nRunning cowyo server (version %s) at https://%s:%s\n\n", version, host, c.String("port"))
 		} else {
-			fmt.Printf("\nRunning cowyo server (version %s) at http://%s:%s\n\n", version, host, c.GlobalString("port"))
+			fmt.Printf("\nRunning cowyo server (version %s) at http://%s:%s\n\n", version, host, c.String("port"))
 		}
 
 		server.Serve(
 			pathToData,
-			c.GlobalString("host"),
-			c.GlobalString("port"),
-			c.GlobalString("cert"),
-			c.GlobalString("key"),
+			c.String("host"),
+			c.String("port"),
+			c.String("cert"),
+			c.String("key"),
 			TLS,
-			c.GlobalString("css"),
-			c.GlobalString("default-page"),
-			c.GlobalString("lock"),
-			c.GlobalInt("debounce"),
-			c.GlobalBool("diary"),
-			c.GlobalString("cookie-secret"),
-			c.GlobalString("access-code"),
-			c.GlobalBool("allow-insecure-markup"),
-			c.GlobalBool("allow-file-uploads"),
-			c.GlobalUint("max-upload-mb"),
-			c.GlobalUint("max-document-length"),
-			logger(c.GlobalBool("debug")),
+			c.String("css"),
+			c.String("default-page"),
+			c.String("lock"),
+			c.Int("debounce"),
+			c.Bool("diary"),
+			c.String("cookie-secret"),
+			c.String("access-code"),
+			c.Bool("allow-insecure-markup"),
+			c.Bool("allow-file-uploads"),
+			c.Uint("max-upload-mb"),
+			c.Uint("max-document-length"),
+			logger(c.Bool("debug")),
 		)
 		return nil
 	}
 	app.Flags = []cli.Flag{
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "data",
 			Value: "data",
 			Usage: "data folder to use",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "olddata",
 			Value: "",
 			Usage: "data folder for migrating",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "host",
 			Value: "",
 			Usage: "host to use",
 		},
-		cli.StringFlag{
-			Name:  "port,p",
-			Value: "8050",
-			Usage: "port to use",
+		&cli.StringFlag{
+			Name:    "port",
+			Aliases: []string{"p"},
+			Value:   "8050",
+			Usage:   "port to use",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "cert",
 			Value: "",
 			Usage: "absolute path to SSL public sertificate",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "key",
 			Value: "",
 			Usage: "absolute path to SSL private key",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "css",
 			Value: "",
 			Usage: "use a custom CSS file",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "default-page",
 			Value: "",
 			Usage: "show default-page/read instead of editing (default: show random editing)",
 		},
-		cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:  "allow-insecure-markup",
 			Usage: "Skip HTML sanitization",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "lock",
 			Value: "",
 			Usage: "password to lock editing all files (default: all pages unlocked)",
 		},
-		cli.IntFlag{
+		&cli.IntFlag{
 			Name:  "debounce",
 			Value: 500,
 			Usage: "debounce time for saving data, in milliseconds",
 		},
-		cli.BoolFlag{
-			Name:  "debug, d",
-			Usage: "turn on debugging",
+		&cli.BoolFlag{
+			Name:    "debug",
+			Aliases: []string{"d"},
+			Usage:   "turn on debugging",
 		},
-		cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:  "diary",
 			Usage: "turn diary mode (doing New will give a timestamped page)",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "access-code",
 			Value: "",
 			Usage: "Secret code to login with before accessing any wiki stuff",
 		},
-		cli.StringFlag{
+		&cli.StringFlag{
 			Name:  "cookie-secret",
 			Value: "secret",
 			Usage: "random data to use for cookies; changing it will invalidate all sessions",
 		},
-		cli.BoolFlag{
+		&cli.BoolFlag{
 			Name:  "allow-file-uploads",
 			Usage: "Enable file uploads",
 		},
-		cli.UintFlag{
+		&cli.UintFlag{
 			Name:  "max-upload-mb",
 			Value: 2,
 			Usage: "Largest file upload (in mb) allowed",
 		},
-		cli.UintFlag{
+		&cli.UintFlag{
 			Name:  "max-document-length",
 			Value: 100000000,
 			Usage: "Largest wiki page (in characters) allowed",
 		},
 	}
-	app.Commands = []cli.Command{
-		{
+	app.Commands = []*cli.Command{
+		&cli.Command{
 			Name:    "migrate",
 			Aliases: []string{"m"},
 			Usage:   "migrate from the old cowyo",
 			Action: func(c *cli.Context) error {
-				pathToData = c.GlobalString("data")
-				pathToOldData := c.GlobalString("olddata")
+				pathToData = c.String("data")
+				pathToOldData := c.String("olddata")
 				if len(pathToOldData) == 0 {
 					fmt.Printf("You need to specify folder with -olddata")
 					return nil
@@ -167,7 +169,7 @@ func main() {
 					fmt.Printf("Can not find '%s', does it exist?", pathToOldData)
 					return nil
 				}
-				server.Migrate(pathToOldData, pathToData, logger(c.GlobalBool("debug")))
+				server.Migrate(pathToOldData, pathToData, logger(c.Bool("debug")))
 				return nil
 			},
 		},
