f6abfa8c14ced86aaa5228aaea6dc41e4f3ab123
Songtao Chu <im@chu.st>
2020-08-13 00:43:26 +0800

    Update static files

--- static/js/cowyo.js
+++ static/js/cowyo.js
@@ -1,7 +1,7 @@
 "use strict";
 var oulipo = false;
 
-$(window).load(function() {
+$(window).on('load',function() {
     // Returns a function, that, as long as it continues to be invoked, will not
     // be triggered. The function will be called after it stops being called for
     // N milliseconds. If `immediate` is passed, trigger the function on the
